1dab4d3b59c3e3f26bd9e28c1d9a30dcb74cda27
Cameron NEPE <cn93@students.waikato.ac.nz>
2020-08-26 19:27:50 +1200

    Add project files.

---
+++ MagicDestroyers/Characters/Melee/Assassin.cs
@@ -0,0 +1,39 @@
+﻿using MagicDestroyers.Equipment.Armors.Leather;
+using MagicDestroyers.Equipment.Weapons.Sharp;
+using System;
+
+namespace MagicDestroyers.Characters.Melee
+{
+    public class Assassin
+    {
+        private int abilityPoints;
+        private int healthPoints;
+        private int level;
+
+        private string faction;
+        private string name;
+
+        private LightLeatherVest bodyArmor;
+        private Sword weapon;
+        
+        public Assassin()
+        {
+
+        }
+
+        public void Raze()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BleedToDeath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Survival()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MagicDestroyers/Characters/Melee/Warrior.cs
@@ -0,0 +1,40 @@
+﻿using MagicDestroyers.Equipment.Armors.Heavy;
+using MagicDestroyers.Equipment.Weapons.Sharp;
+using System;
+
+namespace MagicDestroyers.Characters.Melee
+{
+    public class Warrior
+    {
+        private int abilityPoints;
+        private int healthPoints;
+        private int level;
+
+        private string faction;
+        private string name;
+
+        private Chainlink bodyArmor;
+        private Axe weapon;
+
+        public Warrior()
+        {
+
+        }
+
+        public void Strike()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SkinHarden()
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
